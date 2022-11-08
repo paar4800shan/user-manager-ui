afab6ba02bb57bb110d8088969dba2acdc5c7051
Paargav Shanker <paargav.shuttle@gmail.com>
2022-11-08 17:53:09 +0530

    conflicts

--- src/validators/TransactionManagementValidator.js
+++ src/validators/TransactionManagementValidator.js
@@ -1,39 +1,41 @@
+import { TRANSACTION_TYPE_VALUES } from "../data/ViewStatementForm";
 import {
-    TRANSACTION_TYPE_VALUES,
-  } from "../data/ViewStatementForm";
-  import { validateDropdown, validateEmptyData, validateLoanAmount, validateNegativeAmount } from "./validators";
+  validateDropdown,
+  validateEmptyData,
+  validateTransactionAmount,
+  validateNegativeAmount,
+} from "./validators";
 
 export const validateTransactionManagementForm = (data) => {
-    if (!validateDropdown(data.transactionType, TRANSACTION_TYPE_VALUES)) {
-      return {
-        status: false,
-        message: "Invalid Transaction Type",
-      };
-    }
-  
-  
-    if (!validateLoanAmount(data.loanAmount, data.balance)) {
-      return {
-        status: false,
-        message: "Invalid Amount",
-      };
-    }
-
-    if(!validateNegativeAmount(data.loanAmount)) {
-      return {
-        status: false,
-        message: "Enter an amount greater than 0"
-      }
-    }
-
-    if (!validateEmptyData(data.loanAmount)) {
-      return {
-        status: false,
-        message: "Invalid Amount",
-      };
-    }
-  
+  if (!validateDropdown(data.transactionType, TRANSACTION_TYPE_VALUES)) {
     return {
-      status: true,
+      status: false,
+      message: "Invalid Transaction Type",
     };
+  }
+
+  if (!validateTransactionAmount(data.loanAmount, data.balance)) {
+    return {
+      status: false,
+      message: "Invalid Amount",
+    };
+  }
+
+  if (!validateNegativeAmount(data.loanAmount)) {
+    return {
+      status: false,
+      message: "Enter an amount greater than 0",
+    };
+  }
+
+  if (!validateEmptyData(data.loanAmount)) {
+    return {
+      status: false,
+      message: "Invalid Amount",
+    };
+  }
+
+  return {
+    status: true,
+  };
 };
